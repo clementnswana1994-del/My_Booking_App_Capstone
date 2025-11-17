4fe3626ffff1029dc3609ae0cf12bf501d7bcf4c
clementnswana1994-del <clementnswana1994@gmail.com>
2025-11-17 14:55:53 -0500

    update on app

--- Frontend_booking/src/App.jsx
+++ Frontend_booking/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    async function test() {
+      await fetch('http://localhost:8080/')
+    }
+  }, [])
   const [count, setCount] = useState(0)
 
   return (
