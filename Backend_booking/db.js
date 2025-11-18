b92096f40e1edd5216b20d33f1158f392b02da98
clementnswana1994-del <clementnswana1994@gmail.com>
2025-11-18 13:33:27 -0500

    Mongodb connect

--- Backend_booking/db.js
+++ Backend_booking/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('MongoDB Connected')
     } catch(e) {
         console.log(e)
--- Backend_booking/index.js
+++ Backend_booking/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 
+import 'dotenv/config'
+
+import connectDB from './db.js';
+
 const app = express()
 
 const port = 8080
@@ -13,4 +17,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Listening on port: ' + port)
+    connectDB()
 })
